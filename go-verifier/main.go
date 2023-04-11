38e848469c2819e400e54bbec1e9394a58c1beed
Dylan Sprague <dylan.richard.sprague@gmail.com>
2023-04-11 18:06:12 -0400

    Implement solution verifier in Go

--- go-verifier/main.go
+++ go-verifier/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/DylanSp/n-queens/go-verifier/verifier"
 )
 
 func main() {
-	fmt.Println("Not yet implemented")
+	possibleSolution := verifier.Solution{
+		N: 2,
+		Queens: []verifier.Queen{
+			{
+				Row:    0,
+				Column: 0,
+			},
+			{
+				Row:    1,
+				Column: 1,
+			},
+		},
+	}
+
+	isSolutionCorrect := verifier.VerifySolution(possibleSolution)
+
+	if isSolutionCorrect {
+		fmt.Println("Correct solution :D")
+	} else {
+		fmt.Println("Incorrect solution :(")
+	}
 }
--- go-verifier/verifier/verifier.go
+++ go-verifier/verifier/verifier.go
@@ -1,5 +1,9 @@
 package verifier
 
+import (
+	"math"
+)
+
 type BoardSize uint
 
 type RowNumber uint
@@ -17,5 +21,38 @@ type Solution struct {
 }
 
 func VerifySolution(solution Solution) bool {
-	panic("Not yet implemented")
+	// validate that there are the correct number of queens
+	if len(solution.Queens) < int(solution.N) {
+		return false
+	}
+
+	// loop through all pairs of queens and check if any threaten each other
+	for i, queen1 := range solution.Queens {
+		for _, queen2 := range solution.Queens[(i + 1):] {
+			if queensThreaten(queen1, queen2) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func queensThreaten(queen1 Queen, queen2 Queen) bool {
+	// horizontally
+	if queen1.Row == queen2.Row {
+		return true
+	}
+
+	// vertically
+	if queen1.Column == queen2.Column {
+		return true
+	}
+
+	// diagonally
+	rowDiff := int(queen1.Row) - int(queen2.Row)
+	columnDiff := int(queen1.Column) - int(queen2.Column)
+
+	// take absolute value to account for both diagonal directions
+	return math.Abs(float64(rowDiff)) == math.Abs(float64(columnDiff))
 }
