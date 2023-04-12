9639223cda20e43671ea2a16d57b6695280bc9d9
Dylan Sprague <dylan.richard.sprague@gmail.com>
2023-04-11 20:25:25 -0400

    Add prototype makeSolution() function to verifier
    Doesn't easily work with output from Prolog, though

--- go-verifier/main.go
+++ go-verifier/main.go
@@ -6,9 +6,43 @@ import (
 	"github.com/DylanSp/n-queens/go-verifier/verifier"
 )
 
+func makeSolution(queens [][]uint) verifier.Solution {
+	solution := verifier.Solution{
+		N:      verifier.BoardSize(len(queens)),
+		Queens: []verifier.Queen{},
+	}
+
+	for _, queen := range queens {
+		if len(queen) != 2 {
+			panic(fmt.Sprint("queen is not exactly 2 coordinates; has ", len(queen), " elements"))
+		}
+
+		solution.Queens = append(solution.Queens, verifier.Queen{
+			Row:    verifier.RowNumber(queen[0]),
+			Column: verifier.ColumnNumber(queen[1]),
+		})
+	}
+
+	return solution
+}
+
 func main() {
+	// possibleSolution := verifier.Solution{
+	// 	N: 2,
+	// 	Queens: []verifier.Queen{
+	// 		{
+	// 			Row:    0,
+	// 			Column: 0,
+	// 		},
+	// 		{
+	// 			Row:    1,
+	// 			Column: 1,
+	// 		},
+	// 	},
+	// }
+
 	possibleSolution := verifier.Solution{
-		N: 2,
+		N: 5,
 		Queens: []verifier.Queen{
 			{
 				Row:    0,
@@ -16,7 +50,19 @@ func main() {
 			},
 			{
 				Row:    1,
+				Column: 2,
+			},
+			{
+				Row:    2,
+				Column: 4,
+			},
+			{
+				Row:    3,
 				Column: 1,
+			},
+			{
+				Row:    4,
+				Column: 3,
 			},
 		},
 	}
